b243d26c412525d0cba155be90b5c843c5e8f465
James Smith <james@cybersmith.io>
2022-03-24 11:32:07 -0400

    Simple test for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>Copyright &copy;</div>
     </div>
   );
 }
