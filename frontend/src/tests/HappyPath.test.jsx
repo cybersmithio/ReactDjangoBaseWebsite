17d56f188b6d8be31a061a5c7c8fa7bc45619215
James Smith <james@cybersmith.io>
2022-03-24 13:10:55 -0400

    Adding more tests in Happy Path

--- frontend/src/tests/HappyPath.test.jsx
+++ frontend/src/tests/HappyPath.test.jsx
@@ -6,4 +6,26 @@ test("Happy path test", async () => {
   render(<App />);
   const copyrightText = await screen.findByText(/copyright/i);
   expect(copyrightText).toBeInTheDocument();
+
+  const loginLink = await screen.findByRole("link", { name: /login/i });
+  expect(loginLink).toBeInTheDocument();
+  userEvent.click(loginLink);
+
+  const signInText = await screen.findByRole("heading", { name: /sign in/i });
+  expect(signInText).toBeInTheDocument();
+
+  const emailField = await screen.findByRole("textbox", {
+    name: /email address/i,
+  });
+  expect(emailField).toBeInTheDocument();
+
+  const passwordField = await screen.findByLabelText(/password/i);
+  expect(passwordField).toBeInTheDocument();
+
+  const signInButton = await screen.findByRole("button", { name: /sign in/i });
+  expect(signInButton).toBeInTheDocument();
+  expect(signInButton).toBeDisabled();
+
+  const registerLink = await screen.findByRole("link", { name: /register/i });
+  expect(registerLink).toBeInTheDocument();
 });
