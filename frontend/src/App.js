d63cfdea3161855ce3e83451bb25930d394ac8ae
James Smith <james@cybersmith.io>
2022-03-24 11:50:08 -0400

    Add Reacting Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>Copyright &copy;</div>
-    </div>
+    <Router>
+      <main>
+        <Container>
+          <Route path="/" component={HomePage} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
