9a106254b02de307ff0b6db4471c4b7f2ebed4c8
James Smith <james@cybersmith.io>
2022-03-28 17:27:58 -0400

    Profile Page Shows Loading When Updating

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -56,45 +56,49 @@ function ProfilePage({ history }) {
     setError(false);
   };
 
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>User Profile</h1>
-          {error ? (
-            <Alert variant="danger" style={{ backgroundColor: "red" }}>
-              Unable to update profile. Try again later.
-            </Alert>
-          ) : (
-            <div />
-          )}
+  if (loading) {
+    return <div>Loading</div>;
+  } else {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h1>User Profile</h1>
+            {error ? (
+              <Alert variant="danger" style={{ backgroundColor: "red" }}>
+                Unable to update profile. Try again later.
+              </Alert>
+            ) : (
+              <div />
+            )}
 
-          <Form onSubmit={submitHandler}>
-            <Form.Group controlId="name">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control
-                type="name"
-                placeholder="Enter Full Name"
-                defaultValue={formName}
-                onChange={(e) => setFormName(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="password">
-              <Form.Label>Update Password</Form.Label>
-              <Form.Control type="password" placeholder="Update password" />
-            </Form.Group>
-            <Form.Group controlId="confirmPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Confirm password" />
-            </Form.Group>
-            <Button type="submit" variant="primary">
-              Update
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
+            <Form onSubmit={submitHandler}>
+              <Form.Group controlId="name">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control
+                  type="name"
+                  placeholder="Enter Full Name"
+                  defaultValue={formName}
+                  onChange={(e) => setFormName(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Update Password</Form.Label>
+                <Form.Control type="password" placeholder="Update password" />
+              </Form.Group>
+              <Form.Group controlId="confirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="password" placeholder="Confirm password" />
+              </Form.Group>
+              <Button type="submit" variant="primary">
+                Update
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default ProfilePage;
