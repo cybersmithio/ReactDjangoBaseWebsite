003006a0121ffff5fa02b9689249ac95f98d7269
James Smith <james@cybersmith.io>
2022-03-29 14:10:00 -0400

    Test  Forgot Password Page Sends To Backend

--- frontend/src/constants/urls.js
+++ frontend/src/constants/urls.js
@@ -9,3 +9,6 @@ export const LOGIN_ENDPOINT = BACKEND_URL + "api/users/token/";
 
 export const PROFILE_UPDATE_ENDPOINT =
   BACKEND_URL + "api/users/profile/update/";
+
+export const FORGOT_PASSWORD_ENDPOINT =
+  BACKEND_URL + "api/users/password/forgot/";
