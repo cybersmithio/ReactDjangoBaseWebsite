275362ec1af445c7d7fe8d322b77855c5440d836
James Smith <james@cybersmith.io>
2022-03-31 16:46:14 -0400

    Update Admin Forms For Custom User Module

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+ 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+ 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+ 
+ 
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('name','email', 'is_staff', 'is_active','date_joined','sent_verification_email','verified_email')
+    list_filter = ('name','email', 'is_staff', 'is_active','date_joined','sent_verification_email','verified_email')
+ 
+    fieldsets = (
+        (None, {'fields': ('name','email', 'password','date_joined','sent_verification_email','verified_email')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+ 
+admin.site.register(CustomUser, CustomUserAdmin)
