a9419a6aa093fd3e7f652e5bdf7d37d7e693ef79
James Smith <james@cybersmith.io>
2022-03-31 16:07:57 -0400

    Update Django User Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+ 
+from .managers import CustomUserManager
+ 
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    name = models.CharField(max_length=100)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    sent_verification_email = models.BooleanField(default=False)
+    verified_email = models.BooleanField(default=False)
+ 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+ 
+    objects = CustomUserManager()
+ 
+    def __str__(self):
+        return self.email
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'api.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
