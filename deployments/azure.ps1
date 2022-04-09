287709395ed663f148cecc59f4ed64c9894e2030
James Smith <james@cybersmith.io>
2022-04-08 22:43:55 -0400

    AKS Permissions To ACR

--- deployments/azure.ps1
+++ deployments/azure.ps1
@@ -99,6 +99,76 @@ function DeployAzureObject() {
                 exit 
             }
         }
+        aks {
+            $retval=az aks show -g $rg --name $name 2> $null
+            if ( -not $retval) {
+                $aksSubnetId=az network vnet subnet show `
+                    -g $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].vnetResourceGroup `
+                    --name $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].vnetSubnetName `
+                    --vnet-name $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].vnetName --output json --query id |ConvertFrom-Json
+                if( -not $? ) {
+                    "Unable to find ID for AKS Subnet"
+                    exit
+                }
+                az aks create -g $rg --name $name `
+                    --node-count $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].nodeCount `
+                    --enable-managed-identity `
+                    --enable-cluster-autoscaler `
+                    --max-count $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].maxCount `
+                    --min-count $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].minCount `
+                    -s $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].nodeSize `
+                    --nodepool-name $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].nodePoolName `
+                    --enable-addons monitoring,azure-keyvault-secrets-provider `
+                    --enable-rbac `
+                    --network-plugin $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].networkPlugin `
+                    --vnet-subnet-id $aksSubnetId `
+                    --docker-bridge-address 10.1.0.1/16 `
+                    --dns-service-ip 10.0.0.10 `
+                    --service-cidr 10.0.0.0/16 `
+                    --aci-subnet-name $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].vnetSubnetName `
+                    --dns-name-prefix $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].dnsNamePrefix `
+                    --yes
+                if( -not $? ) {
+                    "Unable to create the AKS cluster"
+                    exit
+                }
+            } else {
+                "AKS already created"
+            }
+            $id=az aks show -g $rg --name $name --output json --query id |ConvertFrom-Json
+            if( -not $? ) { exit }
+        }
+        role {
+            switch( $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].assigneeType ) {
+                aksPool {
+                    $assigneeId=az aks show -g $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].assigneeResourceGroup `
+                        --name $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].assigneeName --query "identityProfile.kubeletidentity.objectId"
+                }
+                default {
+                    "Unknown assignee type: $($config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].assigneeType)"
+                    exit
+                }
+            }
+            "Assignee ID is $assigneeId"
+            switch( $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].scopeType ) {
+                acr {
+                    $scopeId=az acr show -g $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].scopeResourceGroup `
+                        --name $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].scopeName --query id
+                }
+                default {
+                    "Unknown scope type: $($config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].scopeType)"
+                    exit
+                }
+            }
+            "Scope ID is $scopeId"
+            az role assignment create --role $config.azure.resourceGroups[$resourceGroupIndex].objects[$objectIndex].role --assignee $assigneeId --scope $scopeId
+            if( -not $? ) {
+                "Unable to assign role"
+                exit
+            }
+            "Role has been assigned"
+            return
+        }
 
         default {
             "Unknown object type: '$objectType'"
